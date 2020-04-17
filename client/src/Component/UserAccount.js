5422fe58e86a2563c9d74a9db379bbf7503959d8
mona <hibaalaani@github.com>
2020-04-17 09:57:24 +0200

    tried fix backend

--- client/src/Component/UserAccount.js
+++ client/src/Component/UserAccount.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import Axios from "axios";
 import JwtDecode from "jwt-decode";
 import { login } from "../store/actions/usersAction";
+import UserFavoriteItinerary from "./UserFavoriteItinerary";
 // import { fetchUsersAction } from "../store/actions/usersAction";
 class UserAccount extends Component {
   render() {
@@ -13,8 +14,9 @@ class UserAccount extends Component {
         <h1> you are finish the registration </h1>{" "}
         {this.props.users.favorites &&
           this.props.users.favorites.map((favorite) => {
-            return <h1>{favorite.name}</h1>;
+            return <h1>{favorite}</h1>;
           })}
+        <UserFavoriteItinerary />
       </div>
     );
   }
