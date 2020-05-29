0ace75c8d3ca8b121760de11452d03fd5e91b5af
mona <hibaalaani@github.com>
2020-05-30 00:04:20 +0200

    make it better

--- server/production.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://hiba:ilovehajirsarah2020@cluster0-jz6ge.mongodb.net/results?retryWrites=true&w=majority",
-  secretOrKey: "secret",
-  ClientID:
-    "352999401573-jcep0lnj6ujsubt1kanrted2bv96e307.apps.googleusercontent.com",
-  ClientSecret: "NDZEvxFKhQpoijBBXX-_Gzcl",
-};
